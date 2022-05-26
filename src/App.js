131587322004ebfc6926796c485789ba207aa191
Michael Rogers <mrogers23458@gmail.com>
2022-05-26 09:06:01 -0700

    cleared comments

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./App.css"
 
 function App() {
 
-  //error during page reload: Uncaught ReferenceError: process is not defined ?
-
   const url = 'https://dog.ceo/api/breeds/image/random'
 
   return (
