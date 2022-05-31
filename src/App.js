1ce0271b24bc8ddf6c4960836bc3a08285b0f4e4
Caleb Hatch <hatchcaleb@gmail.com>
2022-05-31 12:09:26 -0500

    saving tech challenge for learning material

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import "./App.css"
 
 function App() {
-
   const url = 'https://dog.ceo/api/breeds/image/random'
+  const [newDog, setNewDog] = useState([]);
+
+  // useEffect(() => {
+  //   buttonClick();
+  // },[])
+
+  const buttonClick = async() => {
+    try {
+      const response = await axios.get(url)
+      console.log('Should get a response', response);
+      setNewDog([...newDog, response.data.message]);
+    } catch(err) {
+      console.log('Not getting data properly', err);
+    }}
+
+    console.log('Should have data on click', newDog);
+    
 
   return (
     <div className="App">
       <div className="container">Dogs go here...</div>
+      <button onClick={buttonClick}>Dogs</button>
+      {
+        newDog.map((dog) => {
+          return (
+            <div>
+              <img src={dog}/>
+            </div>
+          );
+        })
+      }
     </div>
   )
 }
